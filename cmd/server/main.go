c95dee5fa6031d3a1eb9ac3983c16e3367ac2247
Konstantin Lapshin <lapshin.k@gmail.com>
2024-10-20 21:39:39 +0300

    test fix 2

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -20,7 +20,7 @@ var (
 func init() {
 	// Подключение логирования
 	logCfg := zap.NewDevelopmentConfig()
-	logCfg.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
+	logCfg.Level = zap.NewAtomicLevelAt(zap.InfoLevel)
 	logger, err = logCfg.Build()
 	if err != nil {
 		log.Fatal("Failed to build logger:", err.Error())
